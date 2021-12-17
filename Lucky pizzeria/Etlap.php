6d76339488b741d9e48838a43347ef20b7195bc8
Trover <74741960+Troversones@users.noreply.github.com>
2021-12-17 10:31:16 +0100

    December 17 10:36
    Navbar class change

--- Lucky pizzeria/Etlap.php
+++ Lucky pizzeria/Etlap.php
@@ -21,7 +21,7 @@
         <div class="row">
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="index.html"><img src="kepek/logofasz.png" class="simage img-fluid" alt="anyas"></a>
+                    <a class="navbar-brand" href="index.html"><img src="kepek/logofasz.png" class="img-fluid simage" alt="anyas"></a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
